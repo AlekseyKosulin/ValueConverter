1f3411e3e2937810857408987fdb9c50e80d51e9
Алексей Косулин <kosylapa@yandex.ru>
2026-07-02 11:18:38 +0300

    Подключил RestAPI

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,8 @@ namespace ValueConverter
 {
     public partial class Form1 : Form
     {
+        RestAPI api = new RestAPI();
+
         public Form1()
         {
             InitializeComponent();
---
+++ RestAPI.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Json;
+
+namespace ValueConverter
+{
+    internal class RestAPI
+    {
+        HttpClient client = new HttpClient();
+
+        public async Task<Dictionary<string, string>> GetCurrencies()
+        {
+            string url = "https://api.frankfurter.app/currencies";
+            Dictionary<string, string> result = await client.GetFromJsonAsync<Dictionary<string, string>>(url);
+            return result;
+        }
+
+        public async Task<decimal> GetRate(string from, string to)
+        {
+            string url = "https://api.frankfurter.app/latest?from=" + from + "&to=" + to;
+            RateResponse result = await client.GetFromJsonAsync<RateResponse>(url);
+            return result.rates[to];
+        }
+    }
+
+    class RateResponse
+    {
+        public Dictionary<string, decimal> rates { get; set; }
+    }
+}
